2f69cff2809b348eccb87b894feef8fadb853b35
Priyanka <priyankaqaz12345@gmail.com>
2022-02-26 21:09:37 +0530

    Completed

--- routes/news.js
+++ routes/news.js
@@ -1,26 +1,24 @@
 const express = require('express')
 const axios = require('axios')
-const newsr=express.Router()
+const newsr = express.Router()
 
-newsr.get('/',async(req,res)=>{
+newsr.get('/', async (req, res) => {
     try {
         var url = 'http://newsapi.org/v2/top-headlines?' +
-          'sources=the-times-of-india&' +
-          'apiKey=5ffb1c9fa153496795d36fc58a64d7ba';
+            'sources=the-times-of-india&' +
+            'pageSize=6&' +
+            'apiKey=5ffb1c9fa153496795d36fc58a64d7ba';
 
-        const news_get =await axios.get(url)
+        const news_get = await axios.get(url)
         console.log(news_get);
-        res.render('index',{articles:news_get.data.articles[0].url})
-
-
-
+        res.render('index', { articles: news_get.data.articles })
 
     } catch (error) {
-        if(error.response){
+        if (error.response) {
             console.log(error)
         }
 
     }
 })
 
-module.exports=newsr
+module.exports = newsr
